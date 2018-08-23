a0efc6f7dc9ef5bfd0efc01708cff8a8e91cffb6
McJevons <jewely@qq.com>
2018-08-23 16:00:37 +0800

    test

--- urllib_urlopen.py
+++ urllib_urlopen.py
@@ -1,5 +1,7 @@
 import urllib.request
 
-url = 'http://www.sh-huasing.com/web10'
-response = urllib.request.urlopen(url)
+data = bytes(urllib.parse.urlencode({'a': 1, 'b': 2}), encoding='utf-8')
+url = 'http://www.sh-huasing.com/web10/online/login.asp'
+req = urllib.request.Request(url, data=data, method='POST')
+response = urllib.request.urlopen(req)
 print(response.read().decode('gb2312'))
