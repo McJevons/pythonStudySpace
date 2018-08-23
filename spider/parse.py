303563d4bc5c730da13b0455c713be78a82f9c06
mcjevons <jewely@qq.com>
2018-08-23 23:49:09 +0800

    finish urllib.parse study

--- spider/parse.py
+++ spider/parse.py
@@ -7,3 +7,33 @@ print(res[0], res.scheme)
 
 res = urllib.parse.urlsplit(url)
 print(res)
+
+unparse = ('http', 'www.baidu.com', 'index.html', 'abc', 'm=1&n=xyz', 'opq')
+new_url = urllib.parse.urlunparse(unparse)
+print(new_url == url)
+
+unsplit = ['http', 'www.baidu.com', 'index.html;abc', 'm=1&n=xyz', 'opq']
+new_url = urllib.parse.urlunsplit(unsplit)
+print(new_url == url)
+
+print(urllib.parse.urljoin('http://www.abc.com/abc.html', 'xyz.html'))
+print(urllib.parse.urljoin('http://www.abc.com/a/b/c/abc.html', 'xyz.html'))
+print(urllib.parse.urljoin('http://www.abc.com/a/b/c/abc.html', '../xyz.html'))
+print(
+    urllib.parse.urljoin('http://www.abc.com/a/b/c/abc.html',
+                         '../../xyz.html'))
+print(
+    urllib.parse.urljoin('http://www.abc.com/a/b/c/abc.html',
+                         '../../../xyz.html'))
+
+data = {'a': 1, 'b': 2}
+data = urllib.parse.urlencode(data)
+print(data)
+
+print(urllib.parse.parse_qs(data))
+print(urllib.parse.parse_qsl(data))
+
+chs = '你好'
+chr_chs = urllib.parse.quote(chs)
+chs = urllib.parse.unquote(chr_chs)
+print(chr_chs, chs, sep='\n')
