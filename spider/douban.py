0f7d0c7a7de21ff4e1bff75d4e92352c8e9079ad
McJevons <jewely@qq.com>
2018-09-21 16:48:03 +0800

    豆瓣

--- spider/douban.py
+++ spider/douban.py
@@ -10,20 +10,23 @@ def get_html(url):
 
 def get_data(html):
     soup = BeautifulSoup(html, 'lxml')
-    # tmp=soup.select('div.info h2 a')
-    # title = [re.sub('\s*', '', x.text) for x in tmp]
-    items = soup.select('.subject-item')
+    get_item(soup.select('.subject-item'))
+
+
+def get_item(items):
     for item in items:
-        title = re.sub('\s*', '', item.select('.info h2 a')[0].text)
+        # title = re.sub('\s*', '', item.select('.info h2 a')[0].text)
+        title = re.sub('\s*', '', item.h2.a.text)
+        # 可能存在没有评分的情况，不一定能取得rating_nums
         star = item.find(class_='rating_nums')
         star = star.text if star else '--'
-        price = re.sub('\s*', '', item.select('.pub')[0].text)
-        price = re.search('[^/{1}]?\d+\.{0,1}\d*元{0,1}.?$', price).group(0)
+        price = re.sub('\s*', '', item.find(class_='pub').text)
+        price = re.search('\d+\.{0,1}\d*元{0,1}$', price).group(0)
         print(title, star, price)
 
 
 if __name__ == '__main__':
-    url = 'https://book.douban.com/tag/儿童文学'
+    url = 'https://book.douban.com/tag/儿童文学?start=0&type=T'
     headers = {
         'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 ' +
