c060dc913a2de95da01ed0b172869e19379959fa
mcjevons <jewely@qq.com>
2018-09-25 21:22:25 +0800

    豆瓣

--- spider/douban.py
+++ spider/douban.py
@@ -3,7 +3,7 @@ import re
 from bs4 import BeautifulSoup
 
 
-def get_html(url):
+def get_html(url, headers):
     req = requests.get(url, headers=headers)
     return req.text
 
@@ -21,16 +21,33 @@ def get_item(items):
         star = item.find(class_='rating_nums')
         star = star.text if star else '--'
         price = re.sub('\s*', '', item.find(class_='pub').text)
-        price = re.search('\d+\.{0,1}\d*元{0,1}$', price).group(0)
+        try:
+            price = re.search('\d+\.{0,1}\d*元{0,1}$', price).group(0)
+        except:
+            price = '--'
         print(title, star, price)
 
 
+def get_tag():
+    '''获取查询关键词'''
+    while True:
+        tag = input('Please input the tag:')
+        if tag.strip() == '':
+            print('Wrong input!')
+        else:
+            return tag
+
+
 if __name__ == '__main__':
-    url = 'https://book.douban.com/tag/儿童文学?start=0&type=T'
     headers = {
         'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 ' +
         '(KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36'
     }
-    html = get_html(url)
-    get_data(html)
+
+    tag = get_tag()
+
+    for page in range(3):
+        page_no = page * 20
+        url = 'https://book.douban.com/tag/%s?start=%d&type=T' % (tag, page_no)
+        get_data(get_html(url, headers))
