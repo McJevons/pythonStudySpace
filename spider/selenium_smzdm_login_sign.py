3b5ae88c6e8cfb66bbec248f1d562775f1adbeaa
McJevons <jewely@qq.com>
2018-10-19 16:46:07 +0800

    什么值得买优化

--- spider/selenium_smzdm_login_sign.py
+++ spider/selenium_smzdm_login_sign.py
@@ -1,23 +1,32 @@
 from selenium import webdriver
 
+# 设置chrome选项为不加载图像
 chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.managed_default_content_settings.images": 2}
 chrome_options.add_experimental_option("prefs", prefs)
 
+# 根据chrome选项驱动开启浏览器
 browser = webdriver.Chrome(chrome_options=chrome_options)
+# 加载网页
 browser.get('http://www.smzdm.com')
 
+# 定位登录按钮，并模拟点击
 button_login = browser.find_element_by_class_name('J_login_trigger')
 button_login.click()
 
+# 点击登录按钮后，在页面生成一个包含登录框的iframe，切换至该iframe
 browser.switch_to_frame('J_login_iframe')
+# 定位用户名和密码输入框，并键入用户名密码
 username = browser.find_element_by_id('username')
 password = browser.find_element_by_id('password')
 username.send_keys('jewely@qq.com')
 password.send_keys('******')
+# 定位登录按钮，模拟点击进行登录
 button_submit = browser.find_element_by_id('login_submit')
 button_submit.click()
 
+# 隐式等待10s，若规定时间内后续节点未加载出来则抛出异常
 browser.implicitly_wait(10)
+# 定位签到按钮，模拟点击完成签到
 sign_button = browser.find_element_by_class_name('J_punch')
 sign_button.click()
